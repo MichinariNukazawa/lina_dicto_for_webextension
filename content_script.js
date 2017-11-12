cb984adc14b15731705e30b2ad31c3c5f88d127d
Michinari Nukazawa <michinari.nukazawa@gmail.com>
2017-11-12 10:50:21 +0900

    cleanup

--- content_script.js
+++ content_script.js
@@ -9,24 +9,6 @@
 
 	init_dictionary();
 
-	let prev = {x:0, y:0, click:0};
-
-	/*
-	   document.addEventListener('mousemove', function(e) {
-	   if(200 < Math.abs(prev.x - e.clientX)){
-	   console.log("x:" + e.clientX);
-	   prev.x = e.clientX;
-	   }
-	   });
-	 */
-
-	document.addEventListener("click", function(e) {
-		console.log(e.target);
-		console.log("x:%d, y:%d", e.clientX, e.clientY);
-		console.log(prev.click);
-		prev.click++;
-	});
-
 	let hover = new Hover();
 	let detector = new Detector();
 
