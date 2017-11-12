740aa3082fef964f385dc92de3e1e5946b85c4f1
Michinari Nukazawa <michinari.nukazawa@gmail.com>
2017-11-12 10:18:42 +0900

    view element

--- hover.js
+++ hover.js
@@ -5,6 +5,7 @@ class Hover{
 		let result_root_element = document.createElement('div');
 		result_root_element.style.cssText =
 			''
+			+ 'display: none;'
 			+ 'top:10px;'
 			+ 'left:10px;'
 			+ 'width:auto;'
@@ -13,15 +14,30 @@ class Hover{
 			+ 'border:2px solid black;'
 			+ 'padding:4px;';
 
-		let prev = {
-			"result_root_element": result_root_element
+		let word_element = document.createElement('div');
+		word_element.style.cssText =
+			''
+			+ 'font-size:40%;'
+			+ "font-family: Consolas, 'Courier New', Courier, Monaco, monospace;";
+		let explanation_element = document.createElement('div');
+		explanation_element.style.cssText =
+			''
+			+ 'font-size:100%;'
+			+ 'display: none;';
+		result_root_element.appendChild(word_element);
+		result_root_element.appendChild(explanation_element);
+
+		let state = {
+			"result_root_element": result_root_element,
+			"word_element": word_element,
+			"explanation_element": explanation_element,
 		};
-		this.prev = prev;
+		this.state = state;
 	}
 
 	append(){
 		//console.log("");
-		document.body.appendChild(this.prev.result_root_element);
+		document.body.appendChild(this.state.result_root_element);
 	}
 
 	get_px_str(value){
@@ -29,22 +45,28 @@ class Hover{
 	}
 
 	callback(x, y, word){
-		let result_element = this.prev.result_root_element;
 		if (word == "") {
-			result_element.style["display"] = "none";
-			result_element.textContent = "";
+			this.state.result_root_element.style["display"] = "none";
+			this.state.word_element.textContent = "";
+			this.state.explanation_element.textContent = "";
 		}else{
-			result_element.style["display"] = "inline-block";
-			result_element.style["left"] = this.get_px_str(x + 10);
-			result_element.style["top"] = this.get_px_str(y + 30);
+			this.state.result_root_element.style["display"] = "inline-block";
+			this.state.result_root_element.style["left"] = this.get_px_str(x + 10);
+			this.state.result_root_element.style["top"] = this.get_px_str(y + 30);
 			//console.log(word + " :" + x + "," + y);
 
 			let item = dictionary_get_item_from_keyword(word);
 			if(item){
-				word += "/" + dictionary_get_explanation_from_item(item);
+				this.state.explanation_element
+					.style["display"] = "block";
+				this.state.explanation_element
+					.textContent = dictionary_get_explanation_from_item(item);
+			}else{
+				this.state.explanation_element
+					.style["display"] = "none";
 			}
 
-			result_element.textContent = "`" + word + "`";
+			this.state.word_element.textContent = "`" + word + "`";
 		}
 	}
 };
