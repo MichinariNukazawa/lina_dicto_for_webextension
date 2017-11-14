32c91b524aaa2c4b6fb3b3b83c775f43a52f63ae
Michinari Nukazawa <michinari.nukazawa@gmail.com>
2017-11-14 23:35:12 +0900

    誕生日メッセージ追加

--- popup/popup_menu.js
+++ popup/popup_menu.js
@@ -1,6 +1,36 @@
 'use strict';
 
+//! http://sukerasparo.com/
+function showAmrilatoPairBirthdayMessage(){
+	let message = null;
+
+	let now = new Date();
+	if(10 == (now.getMonth() + 1) && 10 == now.getDate()){
+		message = '10/10はRinの誕生日です (<a href="http://sukerasparo.com/">http://sukerasparo.com/</a>)';
+	}
+	if(9 == (now.getMonth() + 1) && 26 == now.getDate()){
+		message = '9/26はRukaの誕生日です (<a href="http://sukerasparo.com/">http://sukerasparo.com/</a>)';
+	}
+
+	let message_area_element = document.getElementById('message_area');
+	if(message){
+		console.log(message);
+		message = '<span>メッセージ:' + message + '</span><hr>';
+		message_area_element.style.cssText =
+			''
+			+ 'display: block;';
+		message_area_element.innerHTML = message;
+	}else{
+		message_area_element.style.cssText =
+			''
+			+ 'display: none;';
+		message_area_element.innerHTML = "";
+	}
+}
+
 window.addEventListener( 'load', function(e){
+	showAmrilatoPairBirthdayMessage();
+
 	let checkbox_element = document.getElementById('ld_is_enabled');
 
 	var page = browser.extension.getBackgroundPage();
