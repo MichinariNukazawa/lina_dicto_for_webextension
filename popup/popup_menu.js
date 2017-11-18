72ef9f8428ff6f3807a5a9c46954c5d3b0a0654e
Michinari Nukazawa <michinari.nukazawa@gmail.com>
2017-11-19 02:07:41 +0900

    fix Ctrl+クリックでリンクから新しいタブを開いた際に、元のタブの翻訳表示が無効になる不具合を修正

--- popup/popup_menu.js
+++ popup/popup_menu.js
@@ -34,16 +34,12 @@ window.addEventListener( 'load', function(e){
 	let checkbox_element = document.getElementById('ld_is_enabled');
 
 	var page = browser.extension.getBackgroundPage();
-	checkbox_element.checked = page.getEnabled();
-
-	page.setIcon(checkbox_element.checked);
+	checkbox_element.checked = page.get_is_enabled_current_tab();
 
 	checkbox_element.addEventListener('change', function(e) {
 		console.log("change");
 
-		page.setIcon(checkbox_element.checked);
-
-		page.setEnabled(checkbox_element.checked);
+		page.set_is_enabled_current_tab(checkbox_element.checked);
 	});
 
 }, false);
